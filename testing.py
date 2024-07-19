373ac606e75eb9bc0d3ddf6e6bdbddf072f546ed
Bigkenny27 <kennyliu2727@gmail.com>
2024-07-19 11:17:47 +1000

    updating testing.py, need more

--- testing.py
+++ testing.py
@@ -3,16 +3,18 @@ from Wall import Wall
 import run
 
 
-def map_check(map: Map):
-    map.create_map(3, 5)
-    print("Map made with width 3 and height of 5")
-    print(map.map_state)
+
 
 
 def testcase_1():
+    """
+    This testcase tests the initial construction of the map, and then adding of walls
+    """
     # ------------------- Initialisation ---------------------
     testmap = Map()
-    map_check(testmap)
+    testmap.create_map(3, 5)
+    print("Map made with width 3 and height of 5")
+    print(testmap.map_state)
     testcase_1_prewall_correct_state = [[None, None, None], [None, None, None], [None, None, None], [None, None, None],
                                         [None, None, None]]
 
@@ -31,6 +33,42 @@ def testcase_1():
     print("Map with walls")
     print(testmap.display_map())
 
-if __name__ == '__main__':
+    testcase_1_correct_state = [[wall1, None, None], [None, None, None], [None, wall2, None], [None, None, None],
+                                [None, None, None]]
     
+    assert testmap.map_state == testcase_1_correct_state, print("Testcase 1 Failed")
+    
+    print("-------------------")
+    print("Testcase 1 Passed!")
+    print("-------------------")
+    
+    
+def testcase_2():
+    """
+    testing the functionality of the update map function
+    """
+    # ------------------- Initialisation ---------------------
+    testmap = Map()
+    testmap.create_map(3, 5)
+    print("Map made with width 3 and height of 5")
+    print(testmap.map_state)
+    testcase_1_prewall_correct_state = [[None, None, None], [None, None, None], [None, None, None], [None, None, None],
+                                        [None, None, None]]
+    
+    # Adding walls
+    wall1 = Wall(1,1)   
+    wall2 = Wall(2, 3)
+    testmap.add_wall(wall1)
+    testmap.add_wall(wall2)
+    
+# --------------------- print statement --------------------
+    print("Map with walls")
+    print(testmap.display_map())
+    
+    
+    
+
+
+if __name__ == '__main__':
+    testcase_1()
 
